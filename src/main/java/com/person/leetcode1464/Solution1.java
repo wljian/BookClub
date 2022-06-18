9f6d4c48b03764927e9d718d8e14b96f44078ed9
Charlie Jian <CharlieJian@wistronits.com>
2022-06-18 12:10:27 +0800

    init

---
+++ src/main/java/com/person/leetcode1464/Solution1.java
@@ -0,0 +1,12 @@
+package com.person.leetcode1464;
+
+public class Solution1 {
+
+    public int maxProduct(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
